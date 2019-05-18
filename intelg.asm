57f7e01144ecacfc36faa88296f893a7e754bcba
cahenri <git.cahenri@gmail.com>
2019-05-18 17:02:22 -0300

    Adjust T0 settings
    Set Timer0 to trigger an interrupt each 100us
    +Minor formating adjustments

--- intelg.asm
+++ intelg.asm
@@ -5,7 +5,13 @@ code at 0 ; Reset address
     ljmp    INIT
 code
 
-code at 000Bh ; Interrupt address
+code at 000Bh ; T0 interrupt address
+    ljmp    T0_INT
+code
+
+code at 0050h
+T0_INT:
+    lcall   RELOAD_T0
     reti
 code
 
@@ -25,23 +31,27 @@ LOOP:
     ljmp    LOOP
 
 RELOAD_T0:
+; Crystal used: 16MHz
+; machine-cycle: 12*1/16M = 0.75us
+; 133 * 0.75 ~= 0.1ms
+; 65536 - 133 - 3 = 65400 (0xFF78)
     mov     TCON,#00h        ; T0_OFF
-    mov     TH0,#0AAh
-    mov     TL0,#55h
+    mov     TH0,#0FFh
+    mov     TL0,#78h
     mov     TCON,#10h        ; T0_ON    
     ret
 end
 
 INIT_7SEG_CONST:
 ; bit0: A
-    mov     60h,#C0h         ; 0
-    mov     61h,#F9h         ; 1
-    mov     62h,#A4h         ; 2
-    mov     63h,#B0h         ; 3
+    mov     60h,#0C0h        ; 0
+    mov     61h,#0F9h        ; 1
+    mov     62h,#0A4h        ; 2
+    mov     63h,#0B0h        ; 3
     mov     64h,#99h         ; 4
     mov     65h,#92h         ; 5
     mov     66h,#82h         ; 6
-    mov     67h,#F8h         ; 7
+    mov     67h,#0F8h        ; 7
     mov     68h,#80h         ; 8
     mov     69h,#90h         ; 9
     ret
